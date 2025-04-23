89e03874b4031cc5b69e45045ee217ef1fe38a49
SakamotoKousuke <k024g0065@g.neec.ac.jp>
2025-04-23 18:08:59 +0900

    00_03

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <filesystem>
+#include <format>
+
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0,
+		reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()),
+		NULL, 0);
+
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>
+		(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+std::string ConverString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(),
+		static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>
+		(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+
+	
+}
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+//string->wstring
+std::wstring ConvertString(const std::string& str);
+//string->string
+std::string ConvertString(const std::wstring& str);
+
+
+
 
 
 //ウィンドウプロシージャ
@@ -23,8 +74,14 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 }
 
 
+
+
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	OutputDebugStringA("Hello,Directx!\n");
+
+
+	Log(ConverString(std::format(L"WSTRING{}\n",L"abc")));
 
 
 
@@ -53,7 +110,7 @@ RECT wrc = { 0,0,kClientWidth,kClientHeight };
 
 //クライアント領域をもとに実際のサイズにwrcを変更してもらう
 AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
+//ウィンドウの生成
 HWND hwnd = CreateWindow(
 	wc.lpszClassName,
 	L"CG2",
@@ -90,3 +147,4 @@ while (msg.message != WM_QUIT) {
 
 return 0;
 }
+
